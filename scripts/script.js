f2024e7de3901fe2b9f98b8e215882b136c65938
zarias2020 <62381691+zarias2020@users.noreply.github.com>
2021-07-06 11:44:53 +0300

    мод окно и фиксация скролла

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,56 @@ headerCityButton.addEventListener('click', () => {
 
   localStorage.setItem('lomoda-location', city)
 })
+
+//  мод окно  ============
+
+
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cartModalOpen = () => {
+  cartOverlay.classList.add('cart-overlay-open');
+  disableScroll();
+}
+
+const cartModalClose = () => {
+  cartOverlay.classList.remove('cart-overlay-open');
+  enableScroll();
+}
+
+subheaderCart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', (e) => {
+  const target = e.target;
+
+  target.classList.contains("cart__btn-close") ? cartModalClose() : null
+  target.matches(".cart-overlay-open") ? cartModalClose() : null
+});
+
+//  блокировка скролла ===================
+
+const disableScroll = () => {
+
+  const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+  document.body.dbScrollY = window.scrollY;
+
+  document.body.style.cssText = `
+    position: fixed;
+    top: ${-window.scrollY}px;
+    left:0;
+    width:100%;
+    height: 100vh;
+    overflow: hidden;
+    padding-right: ${widthScroll}px;
+  `;
+};
+
+const enableScroll = () => {
+  document.body.style.cssText = '';
+  window.scroll({
+    top: document.body.dbScrollY,
+  })
+};
+
+// ===========================================================
