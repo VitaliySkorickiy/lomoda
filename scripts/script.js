d9c6143611ebb6ada0ec20aafbc5c574809db7dd
zarias2020 <62381691+zarias2020@users.noreply.github.com>
2021-07-08 18:17:56 +0300

    список товаров и заголовок блока

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@
 
 const headerCityButton = document.querySelector('.header__city-button');
 
+let hash = location.hash.substring(1);
+
 if (localStorage.getItem('lomoda-location')) {
   headerCityButton.textContent = localStorage.getItem('lomoda-location');
 }
@@ -28,7 +30,11 @@ const cartOverlay = document.querySelector('.cart-overlay');
 
 const disableScroll = () => {
 
+  if (document.disableScroll) return;
+
   const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+  document.disableScroll = true;
 
   document.body.dbScrollY = window.scrollY;
 
@@ -44,6 +50,8 @@ const disableScroll = () => {
 };
 
 const enableScroll = () => {
+  document.disableScroll = false;
+
   document.body.style.cssText = '';
   window.scroll({
     top: document.body.dbScrollY,
@@ -62,6 +70,38 @@ const cartModalClose = () => {
   enableScroll();
 }
 
+// ===========================================================
+
+//    Запрос БД  ==============================
+
+const getData = async () => {
+  const data = await fetch('db.json')
+
+  if (data.ok) {
+    return data.json()
+  } else {
+    throw new Error(`Данные не получены ${data.status} ${data.statusText}`)
+  }
+
+}
+
+const getGoods = (callback, value) => {
+
+  getData()
+    .then(data => {
+      if (value) {
+        callback(data.filter(item => item.category === value))
+      } else {
+        callback(data);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    })
+}
+
+// ====================================================================
+
 subheaderCart.addEventListener('click', cartModalOpen);
 
 cartOverlay.addEventListener('click', (e) => {
@@ -71,5 +111,65 @@ cartOverlay.addEventListener('click', (e) => {
   target.matches(".cart-overlay-open") ? cartModalClose() : null
 });
 
+//  страница товаров  ===========================
 
-// ===========================================================
+try {
+  const goodsList = document.querySelector('.goods__list');
+
+  if (!goodsList) {
+    throw 'not goods'
+  }
+
+  const goodsTitle = document.querySelector('.goods__title');
+
+  const changeTitle = () => {
+    goodsTitle.textContent = document.querySelector(`[href*="#${hash}"]`).textContent;
+  }
+
+  const createCard = ({ id, preview, cost, brand, name, sizes }) => {
+
+    const li = document.createElement('li');
+
+    li.classList.add('goods__item');
+
+    li.innerHTML = `
+      <article class="good">
+          <a class="good__link-img" href="card-good.html#${id}">
+              <img class="good__img" src="goods-image/${preview}" alt="">
+          </a>
+          <div class="good__description">
+              <p class="good__price">${cost} &#8381;</p>
+              <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+              ${sizes ?
+        `<p class="good__sizes"> Размеры (RUS):<span class="good__sizes-list">${sizes.join(' ')}</span></p>` :
+        ''}
+              <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+          </div>
+      </article>
+    `;
+
+    return li
+  }
+
+  const renderGoodsList = data => {
+    goodsList.textContent = '';
+
+    data.forEach(elem => {
+      const card = createCard(elem);
+      goodsList.append(card);
+    });
+
+  }
+
+  window.addEventListener('hashchange', () => {
+
+    hash = location.hash.substring(1);
+    getGoods(renderGoodsList, hash);
+    changeTitle();
+  })
+  changeTitle();
+  getGoods(renderGoodsList, hash);
+
+} catch (error) {
+  console.log(error);
+}
